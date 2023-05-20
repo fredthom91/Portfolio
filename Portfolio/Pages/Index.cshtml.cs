6d811f02e93c7d0c5cc7ee6123e7b598ddb5bcef
Fredrik Thomasson <fredde.thomasson@gmail.com>
2023-05-21 00:19:29 +0200

    tested api

--- Portfolio/Pages/Index.cshtml.cs
+++ Portfolio/Pages/Index.cshtml.cs
@@ -1,20 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjectsAPI;
+using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
+using ProjectsAPI.Controllers;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.Xml;
+using System;
+using System.Text.Json;
+using Newtonsoft.Json;
 
 namespace Portfolio.Pages
 {
-    public class IndexModel : PageModel
+	[BindProperties]
+	public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
-        }
+		}
 
-        public void OnGet()
-        {
+		
+		public void OnGet()
+		{
+			
+		}
 
-        }
-    }
+
+	}
 }
---
+++ ProjectsAPI/Controllers/ProjectController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
+using Newtonsoft.Json;
+
+
+namespace ProjectsAPI.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class ProjectController : ControllerBase
+	{
+		private readonly List<Project> _projects = new List<Project>
+	{
+		new Project { Id = 1, Name = "BankWebApp", Description = "A bank intern website for bank-workers(admin and cashier), you can see info about customers and users, handle their accounts and transactions aswell as change info", CreatedDate = "2020-01-01" },
+		new Project { Id = 2, Name = "HotelBookingApp", Description = "A console-application for a hotel, you can book and handle customers aswell as rooms etc.", CreatedDate = "2020-01-01" },
+		new Project { Id = 3, Name = "Ad API", Description = "A rest-API for ads.", CreatedDate = "2020-01-01" },
+		new Project { Id = 4, Name = "Fixxo. E-Commerce", Description = "A E-commerce site for clothes.", CreatedDate = "2020-01-01" },
+	};
+
+		[HttpGet]
+		public ActionResult<IEnumerable<Project>> GetProjects()
+		{
+			return _projects.ToList();
+		}
+
+
+	}
+}
